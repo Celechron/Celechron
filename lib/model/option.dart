c21ce91abb7a56b27609543532afe5a7286fe252
nosig <yhszj2013@hotmail.com>
2024-01-28 19:29:36 +0800

    重构设置，修复可用的工作时段保存问题

---
+++ lib/model/option.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class Option {
+  Rx<Duration> workTime;
+  Rx<Duration> restTime;
+  RxMap<DateTime, DateTime> allowTime;
+  RxInt gpaStrategy;
+
+  Option({
+    required this.workTime,
+    required this.restTime,
+    required this.allowTime,
+    required this.gpaStrategy
+  });
+}
