20fb7f458c113796fbf10743ccd6042a3089fa57
nosig <yhszj2013@hotmail.com>
2024-05-01 15:21:07 +0800

    修改版本号(之前忘记推送)

--- lib/model/fuse.dart
+++ lib/model/fuse.dart
@@ -8,7 +8,7 @@ class Fuse {
   late DateTime lastUpdateTime;
 
   final bool isBeta = true;
-  final version = [0, 2, 5];
+  final version = [0, 2, 6];
   List<int>? remoteVersion;
   bool hasNewVersion = false;
 
